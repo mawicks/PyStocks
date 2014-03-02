1ef0b812b875cda0d88b21d1ccbe72d98069c6ab
Mark Wicks <mawicks@gmail.com>
2014-03-01 21:15:26 -0500

    Add pricesource module.

---
+++ pricesource.py
@@ -0,0 +1,31 @@
+import datetime
+
+class StockDB:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def load_to_date(self, symbol, number, end_date=datetime.date.today()):
+        with self.connection.cursor() as cursor:
+            cursor.execute("SELECT h.date,h.close,h.daily_return "
+                           " FROM symbols s LEFT JOIN history h "
+                           " ON (s.id = h.symbol_id) "
+                           " WHERE s.symbol = %s "
+                           "   AND h.date <= %s "
+                           " ORDER by h.date DESC "
+                           " LIMIT %s ",
+                           (symbol, end_date, number))
+            result = reversed(cursor.fetchall())
+        return result
+        
+    def load_date_range(self, symbol, start_date, end_date=datetime.date.today()):
+        with self.connection.cursor() as cursor:
+            cursor.execute("SELECT h.date,h.close,h.daily_return "
+                           " FROM symbols s LEFT JOIN history h "
+                           " ON (s.id = h.symbol_id) "
+                           " WHERE s.symbol = %s "
+                           "   AND h.date >= %s "
+                           "   AND h.date <= %s "
+                           " ORDER by h.date ",
+                           (symbol, start_date, end_date))
+            result = cursor.fetchall()
+        return result
